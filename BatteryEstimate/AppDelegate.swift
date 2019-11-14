f29b3240ec5d240f6a3859cdebf33d3ee45bd1fe
NafeeJ <nafeejan2@gmail.com>
2019-11-14 09:21:56 -0500

    Initial Commit

---
+++ BatteryEstimate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BatteryEstimate
+//
+//  Created by Nafee Jan on 11/14/19.
+//  Copyright © 2019 Nafee Workshop. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BatteryEstimate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BatteryEstimate
+//
+//  Created by Nafee Jan on 11/14/19.
+//  Copyright © 2019 Nafee Workshop. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
