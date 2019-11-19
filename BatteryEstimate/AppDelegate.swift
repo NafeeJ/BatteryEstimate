6566ec5f49f4a4b9e15807c5f1d060d5adc5aa48
NafeeJ <nafeejan2@gmail.com>
2019-11-19 03:45:27 -0500

    No longer visible in Dock, can quit from menu bar

--- BatteryEstimate/AppDelegate.swift
+++ BatteryEstimate/AppDelegate.swift
@@ -12,26 +12,62 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-    var batteryEstimate = BatteryEstimate()
     var updateTimer: Timer?
+    var menu: NSMenu?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
-        updateTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateEstimate), userInfo: nil, repeats: true)
+        //Create menu
+        menu = NSMenu()
+        menu!.addItem(NSMenuItem(title: "Quit BatteryEstimate", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        statusItem.menu = menu
         
+        //Not show in Dock
+        NSApp.setActivationPolicy(.accessory)
+        
+        //Update time remaining
+        updateTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(updateEstimate), userInfo: nil, repeats: true)
         RunLoop.current.add(updateTimer!, forMode: RunLoop.Mode.common)
-        
-        //⚡︎ official high voltage emoji
-        //ϟ some symbol that looks like high voltage
-        statusItem.button?.title = "⚡︎ " + batteryEstimate.getTimeRemaining()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         
     }
     
+    //Set menu bar title to current time remaining
     @objc func updateEstimate() {
-        statusItem.button?.title = "⚡︎ " + batteryEstimate.getTimeRemaining()
+        
+        //⚡︎ official high voltage emoji
+        //ϟ some symbol that looks like high voltage
+        statusItem.button?.title = "⚡︎ " + getTimeRemaining()
+    }
+    
+    //returns esimated battery remaining as String in format HH:MM or other status
+    @objc func getTimeRemaining() -> String {
+        let remainingSeconds: Double = IOPSGetTimeRemainingEstimate()
+        
+        if (remainingSeconds == kIOPSTimeRemainingUnknown) {
+            return "Calculating"
+        }
+        
+        //Later add feature that tells if its charging or plugged in and not charging
+        if (remainingSeconds == kIOPSTimeRemainingUnlimited) {
+            return "AC"
+        }
+        
+        if (remainingSeconds > 0.0) {
+            let remainingMinutes: Double = remainingSeconds / 60
+            let formattedHours: Int = Int(floor(remainingMinutes / 60))
+            let formattedMinutes: Int = Int(remainingMinutes) % 60
+            
+            if (formattedMinutes < 10) {
+                return String(formattedHours) + ":" + "0" + String(formattedMinutes)
+            }
+            
+            return String(formattedHours) + ":" + String(formattedMinutes)
+        }
+        
+        return "Broken"
     }
 }
 
